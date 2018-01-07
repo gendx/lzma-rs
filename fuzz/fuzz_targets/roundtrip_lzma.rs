92f673a64d1b4d995b7f172bdc8ec6cda61f62ae
G. Endignoux <ggendx@gmail.com>
2018-01-07 15:34:04 +0100

    Add more fuzz targets.

--- fuzz/fuzz_targets/roundtrip_lzma.rs
+++ fuzz/fuzz_targets/roundtrip_lzma.rs
@@ -5,18 +5,17 @@ extern crate lzma;
 
 use lzma::error::Result;
 
-fn round_trip(x: &[u8]) -> Result<Vec<u8>> {
+fn round_trip_lzma(x: &[u8]) -> Result<Vec<u8>> {
     let mut compressed: Vec<u8> = Vec::new();
     lzma::lzma_compress(&mut std::io::BufReader::new(x), &mut compressed)?;
     let mut bf = std::io::BufReader::new(compressed.as_slice());
 
     let mut decomp: Vec<u8> = Vec::new();
-    lzma::lzma_decompress(&mut bf, &mut decomp).expect("Can't decompress what we just compressed");
+    lzma::lzma_decompress(&mut bf, &mut decomp)?;
     Ok(decomp)
 }
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(decomp) = round_trip(data) {
-        assert_eq!(decomp, data)
-    }
+    let decomp = round_trip_lzma(data).expect("Can't decompress what we just compressed");
+    assert_eq!(decomp, data);
 });
---
+++ fuzz/fuzz_targets/roundtrip_lzma2.rs
@@ -0,0 +1,21 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate lzma;
+
+use lzma::error::Result;
+
+fn round_trip_lzma2(x: &[u8]) -> Result<Vec<u8>> {
+    let mut compressed: Vec<u8> = Vec::new();
+    lzma::lzma2_compress(&mut std::io::BufReader::new(x), &mut compressed)?;
+    let mut bf = std::io::BufReader::new(compressed.as_slice());
+
+    let mut decomp: Vec<u8> = Vec::new();
+    lzma::lzma2_decompress(&mut bf, &mut decomp)?;
+    Ok(decomp)
+}
+
+fuzz_target!(|data: &[u8]| {
+    let decomp = round_trip_lzma2(data).expect("Can't decompress what we just compressed");
+    assert_eq!(decomp, data);
+});
