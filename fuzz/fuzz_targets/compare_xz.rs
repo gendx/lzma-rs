a6b317bb6196487d7d2a4d36c73441f1165c29ab
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2020-04-16 17:41:04 +0200

    Address compiler warning

--- fuzz/fuzz_targets/compare_xz.rs
+++ fuzz/fuzz_targets/compare_xz.rs
@@ -16,7 +16,7 @@ fn decode_xz_lzmars(compressed: &[u8]) -> Result<Vec<u8>> {
 }
 
 fn decode_xz_xz2(compressed: &[u8]) -> Result<Vec<u8>> {
-    let mut bf = std::io::Cursor::new(compressed);
+    let bf = std::io::Cursor::new(compressed);
     let mut decomp: Vec<u8> = Vec::new();
     // create new XZ decompression stream with 8Gb memory limit and checksum verification disabled
     let xz_stream = stream::Stream::new_stream_decoder(8589935000, stream::IGNORE_CHECK).expect("Failed to create stream");
