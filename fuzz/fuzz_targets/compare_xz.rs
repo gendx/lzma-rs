c22dd7cde102615f38c439039460df1bcf5618be
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2020-04-16 17:33:39 +0200

    Fix XZ result comparison fuzz target

--- fuzz/fuzz_targets/compare_xz.rs
+++ fuzz/fuzz_targets/compare_xz.rs
@@ -4,6 +4,7 @@ extern crate libfuzzer_sys;
 extern crate lzma_rs;
 extern crate xz2;
 
+use std::io::Read;
 use lzma_rs::error::Result;
 use xz2::stream;
 
@@ -18,8 +19,8 @@ fn decode_xz_xz2(compressed: &[u8]) -> Result<Vec<u8>> {
     let mut bf = std::io::Cursor::new(compressed);
     let mut decomp: Vec<u8> = Vec::new();
     // create new XZ decompression stream with 8Gb memory limit and checksum verification disabled
-    let xz_stream = Stream::new_stream_decoder(8589935000, IGNORE_CHECK);
-    xz2::bufread::XzDecoder::new_stream(b, xz_stream).read_to_end(&mut decomp)?;
+    let xz_stream = stream::Stream::new_stream_decoder(8589935000, stream::IGNORE_CHECK).expect("Failed to create stream");
+    xz2::bufread::XzDecoder::new_stream(bf, xz_stream).read_to_end(&mut decomp)?;
     Ok(decomp)
 }
 
@@ -27,9 +28,9 @@ fuzz_target!(|data: &[u8]| {
     let result_lzmars = decode_xz_lzmars(data);
     let result_xz2 = decode_xz_xz2(data);
     match (result_lzmars, result_xz2) {
-        Err, Err => (), // both failed, so behavior matches
-        Ok, Err => panic!("lzma-rs succeeded but xz2 failed"),
-        Err, Ok => panic!("xz2 succeeded but lzma-rs failed"),
-        Ok(a), Ok(b) => assert!(a.as_slice() == b.as_slice())
+        (Err(_), Err(_)) => (), // both failed, so behavior matches
+        (Ok(_), Err(_)) => panic!("lzma-rs succeeded but xz2 failed"),
+        (Err(_), Ok(_)) => panic!("xz2 succeeded but lzma-rs failed"),
+        (Ok(a), Ok(b)) => assert!(a.as_slice() == b.as_slice())
     }
 });
