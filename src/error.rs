c34425bfddc61de80dbe5ce260e8b3a08e20e5f0
Simon Dugas <simon.dugas@cyber.gc.ca>
2020-07-14 21:49:55 +0000

    error: implement Display
    Useful for printing out error messages to users.

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,6 @@
 //! Error handling.
 
+use std::fmt::Display;
 use std::io;
 use std::result;
 
@@ -20,5 +21,40 @@ pub type Result<T> = result::Result<T, Error>;
 impl From<io::Error> for Error {
     fn from(e: io::Error) -> Error {
         Error::IOError(e)
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::IOError(e) => write!(fmt, "io error: {}", e),
+            Error::LZMAError(e) => write!(fmt, "lzma error: {}", e),
+            Error::XZError(e) => write!(fmt, "xz error: {}", e),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Error;
+
+    #[test]
+    fn test_display() {
+        assert_eq!(
+            Error::IOError(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "this is an error"
+            ))
+            .to_string(),
+            "io error: this is an error"
+        );
+        assert_eq!(
+            Error::LZMAError("this is an error".to_string()).to_string(),
+            "lzma error: this is an error"
+        );
+        assert_eq!(
+            Error::XZError("this is an error".to_string()).to_string(),
+            "xz error: this is an error"
+        );
     }
 }
