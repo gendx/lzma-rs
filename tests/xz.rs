9180efe63a66b287ece96b56bf27aa9c4bc038b7
Quentin Gliosca <quentin.gliosca@gmail.com>
2020-12-02 19:35:12 +0100

    Add good-1-lzma2-4 test file

--- tests/xz.rs
+++ tests/xz.rs
@@ -68,6 +68,7 @@ fn big_file() {
     #[cfg(feature = "enable_logging")]
     let _ = env_logger::try_init();
     decomp_big_file("tests/files/foo.txt.xz", "tests/files/foo.txt");
+    decomp_big_file("tests/files/good-1-lzma2-4.xz", "tests/files/good-1-lzma2-4");
 }
 
 #[test]
